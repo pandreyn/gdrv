174d0b14c7b768827baa74592eb328005b0b522e
andrey <pandreyn@gmail.com>
2016-01-11 00:53:35 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,29 @@ var router = express.Router();
 var gdrv = require('../lib/googledrvapi');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   var items = null;
 
-  gdrv.init().then(function(){
-    gdrv.readFile(gdrv.TokenPath).then(
-        function(token){
-          gdrv.authorize(token);
-          items = gdrv.listFiles();
-        },
-        function(err){
-          var authUrl = gdrv.getTokenUrl('http://localhost:3000/oauth2callback');
-          return res.redirect(authUrl);
-        }
-    );
-  });
+  gdrv.init()
+      .then(function () {
+        return gdrv.readFile(gdrv.TokenPath);
+      })
+      .then(function (token) {
+            return gdrv.authorize(token);
+          },
+          function (err) {
+            var authUrl = gdrv.getTokenUrl('http://localhost:3000/oauth2callback');
+            res.redirect(authUrl);
+            throw err;
+          })
+      .then(function () {
+        items = gdrv.listFiles();
 
-  res.render('index', { title: 'Express', items: items });
+        res.render('index', {title: 'Express', items: items});
+      });
+
+
 });
 
 module.exports = router;
--- routes/oauth2callback.js
+++ routes/oauth2callback.js
@@ -7,12 +7,29 @@ var gdrv = require('../lib/googledrvapi');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  var items = null;
   var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
 
-  console.log('query = ', query);
+  //console.log('query = ', query);
 
-  res.render('index', { title: 'Express' });
+  if (query && query.code) {
+    gdrv.saveToken(query.code).then(
+        function(token){
+          gdrv.storeToken(token);
+          gdrv.authorize(token);
+          items = gdrv.listFiles();
+
+          var locals = { title: 'Express', items: items  };
+          res.render('index', locals);
+        }, function (err){
+          var locals = { title: 'Express', err: err  };
+          res.render('index', locals);
+        }
+    );
+  }
+
+
 });
 
 module.exports = router;
